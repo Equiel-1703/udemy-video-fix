1906fedff565976012c841967f473c35e00c9f72
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-11-25 19:27:51 -0300

    update: reapply UI fix when next/previous video is loaded

--- udemy-video-fix.user.js
+++ udemy-video-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Udemy Fix Video Controls
 // @namespace    https://github.com/Equiel-1703
-// @version      1.0
+// @version      1.1
 // @description  Fix stupid idiot video controls of Udemy not disappearing.
 // @author       Henrique Rodrigues Barraz
 // @license 	 GPL-3.0
@@ -21,6 +21,15 @@
 		document.querySelector("head").appendChild(newStyleSection);
 	}
 
+	// New CSS classes
+	const animateOpacity = "animate-opacity";
+	const animateBottom = "animate-bottom";
+	const noCursor = "no-cursor";
+	const cursorHover = "cursor-hover";
+	const hideMouse = "hide-mouse";
+	const noBottomSpace = "no-bottom-space";
+
+	// New CSS code to be added
 	const newCSS = `
 	.animate-opacity {
 		transition-duration: 0.7s;
@@ -52,37 +61,16 @@
 	// Add this new CSS to the <head> of the page
 	addCSS(newCSS);
 
-	// New CSS classes
-	const animateOpacity = "animate-opacity";
-	const animateBottom = "animate-bottom";
-	const noCursor = "no-cursor";
-	const cursorHover = "cursor-hover";
-	const hideMouse = "hide-mouse";
-	const noBottomSpace = "no-bottom-space";
-	
-	// Elements Classes
+	// Elements Classes to be found and manipulated
 	const controlBarClass = "div.shaka-control-bar--control-bar-container--OfnMI";
 	const videoUIElementsClass = "div.user-activity--hide-when-user-inactive--Oc6Cn";
 	const videoContainerClass = "div.curriculum-item-view--content--aaJOw";
 	const captionsClass = "div.captions-display--captions-container--PqdGQ";
 
-	// Now, we need to wait until the video player div appears
-	const observer = new MutationObserver((_mutationsList, observer) => {
-		let containerDiv = document.querySelector(videoContainerClass);
-		let controlBarDiv = document.querySelector(controlBarClass);
+	// Flag to check if the video changes are being monitored or not
+	let monitoringVideoChanges = false;
 
-		if (containerDiv && controlBarDiv) {
-			console.log("UdemyVideoFix> Video container found: ", containerDiv);
-			console.log("UdemyVideoFix> Control bar found: ", controlBarDiv);
-
-			observer.disconnect(); // Stop observing once the div is found
-			fixUdemyVideoControls();
-		}
-	});
-	
-	// Start observing the body for child additions
-	observer.observe(document.body, { childList: true, subtree: true });
-
+	// Function to fix the video controls
 	const fixUdemyVideoControls = () => {
 		// Get all video ui (progress bar, title etc)
 		const videoUIElements = document.querySelectorAll(videoUIElementsClass + ", " + controlBarClass)
@@ -187,4 +175,69 @@
 			}
 		});
 	};
+
+	// This function will monitor the body to check when the video elements are loaded, then fix the video controls
+	const monitorElementsLoadAndFix = () => {
+		const observer = new MutationObserver((_mutationsList, observer) => {
+			let videoContainerDiv = document.querySelector(videoContainerClass);
+			let controlBarDiv = document.querySelector(controlBarClass);
+
+			// Check if the divs were loaded
+			if (videoContainerDiv && controlBarDiv) {
+				console.log("UdemyVideoFix> Video container found: ", videoContainerDiv);
+				console.log("UdemyVideoFix> Control bar found: ", controlBarDiv);
+
+				observer.disconnect(); // Stop observing once the divs are found
+				
+				// Fix the video controls
+				fixUdemyVideoControls();
+
+				// Start monitoring for next and previous video changes if not already monitoring
+				if (!monitoringVideoChanges)
+					monitorNextPreviousVideo();
+			}
+		});
+		
+		// Start observing the body to check when the divs are loaded
+		observer.observe(document.body, { childList: true, subtree: true });
+	};
+
+	// This function will monitor the video container div to check when the video changes
+	const monitorNextPreviousVideo = () => {
+		// Get the video container div
+		const videoContainerDiv = document.querySelector(videoContainerClass);
+
+		// Check if the div was found
+		if (videoContainerDiv == null) {
+			console.log("UdemyVideoFix> Video container not found.");
+			return;
+		} else {
+			console.log("UdemyVideoFix> Monitoring for next and previous video changes.");
+		}
+
+		// Observer to check when the video container changes
+		const observer = new MutationObserver((mutationsList, _observer) => {
+			mutationsList.forEach((mutation) => {
+				if (mutation.type === "childList") {
+					mutation.addedNodes.forEach((node) => {
+						if (node.nodeName === "SECTION") {
+							console.log("UdemyVideoFix> Video changed. Fixing new video controls.");
+		
+							// Restart monitoring for video elements load and then fix the video controls
+							monitorElementsLoadAndFix();
+						}
+					});
+				}
+			});
+		});
+
+		// Start observing the video container div
+		observer.observe(videoContainerDiv, { childList: true, subtree: false });
+
+		// Set the flag to true
+		monitoringVideoChanges = true;
+	};
+
+	// Start monitoring for video elements load
+	monitorElementsLoadAndFix();
 })();
